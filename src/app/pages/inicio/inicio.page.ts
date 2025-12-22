0aa745a9c51f3d16d4da6e4f40cb9ed4b96bb8e4
Angel David <angeldivad.100089@gmail.com>
2025-12-22 13:58:36 -0600

    init pantallas

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonRouterLink } from '@ionic/angular/standalone';
 import { RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-inicio',
   templateUrl: 'inicio.page.html',
   styleUrls: ['inicio.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, RouterLinkActive],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonButton,
+    IonRouterLink,
+    RouterLinkActive
+  ],
 })
 export class inicioPage {
   constructor() {}
