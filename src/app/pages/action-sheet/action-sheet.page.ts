aa315236b05007cf65dae5f9db7833e584fd1de3
Angel David <angeldivad.100089@gmail.com>
2025-12-22 17:52:17 -0600

    init headers

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonBackButton } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { ComponentsModule } from "src/app/components/components-module";
 
 @Component({
   selector: 'app-action-sheet',
   templateUrl: './action-sheet.page.html',
   styleUrls: ['./action-sheet.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonBackButton, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonBackButton, CommonModule, FormsModule, ComponentsModule]
 })
 export class ActionSheetPage implements OnInit {
 
