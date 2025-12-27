733f40660d3a332c4087a180239ad319f7ba7adf
Angel David <angeldivad.100089@gmail.com>
2025-12-27 12:53:46 -0600

    FAB

--- src/app/pages/fab/fab.page.ts
+++ src/app/pages/fab/fab.page.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonFab } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
-import { IonList,IonItem,IonLabel } from "@ionic/angular/standalone";
+import { IonList, IonItem, IonLabel } from "@ionic/angular/standalone";
 import { IonFabButton, IonIcon, IonFabList } from '@ionic/angular/standalone';
-
+import { addIcons } from 'ionicons';
+import { addOutline, logoIonic, logoAngular } from 'ionicons/icons';
 
 @Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-fab',
   templateUrl: './fab.page.html',
   styleUrls: ['./fab.page.scss'],
   standalone: true,
-  imports: [IonContent,
+  imports: [
+    IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
@@ -21,17 +24,21 @@ import { IonFabButton, IonIcon, IonFabList } from '@ionic/angular/standalone';
     HeaderComponent,
     IonList,
     IonItem,
-    IonLabel, IonFooter, IonFab,
+    IonLabel,
+    IonFooter,
+    IonFab,
     IonFabButton,
     IonIcon,
     IonFabList
-    ]
+  ]
 })
 export class FabPage implements OnInit {
 
   data = Array(100);
 
-  constructor() { }
+  constructor() {
+    addIcons({ addOutline, logoIonic, logoAngular });
+  }
 
   ngOnInit() {
   }
