c4069a2a6dd4a6b9107f6e7595cc4d1d1b7fece7
Angel David <angeldivad.100089@gmail.com>
2025-12-27 12:51:37 -0600

    FAB

--- src/app/pages/fab/fab.page.ts
+++ src/app/pages/fab/fab.page.ts
@@ -1,25 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonFab } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { IonList,IonItem,IonLabel } from "@ionic/angular/standalone";
+import { IonFabButton, IonIcon, IonFabList } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-fab',
   templateUrl: './fab.page.html',
   styleUrls: ['./fab.page.scss'],
   standalone: true,
-  imports: [IonContent, 
-            IonHeader, 
-            IonTitle, 
-            IonToolbar, 
-            CommonModule, 
-            FormsModule,
-            HeaderComponent,
-            
-          ]
+  imports: [IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    IonList,
+    IonItem,
+    IonLabel, IonFooter, IonFab,
+    IonFabButton,
+    IonIcon,
+    IonFabList
+    ]
 })
 export class FabPage implements OnInit {
+
+  data = Array(100);
 
   constructor() { }
 
