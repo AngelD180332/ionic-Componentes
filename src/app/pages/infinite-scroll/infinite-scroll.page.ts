e49f0edbfe78417807ffcd034283b0f92c112565
Angel David <angeldivad.100089@gmail.com>
2025-12-29 14:51:37 -0600

    infinite scroll

--- src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -1,30 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel } from '@ionic/angular/standalone';
 import { IonInfiniteScroll } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { IonList,IonItem } from '@ionic/angular/standalone';
+import { IonInfiniteScrollContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-infinite-scroll',
   templateUrl: './infinite-scroll.page.html',
   styleUrls: ['./infinite-scroll.page.scss'],
   standalone: true,
-  imports: [IonContent, 
-            IonHeader, 
-            IonTitle, 
-            IonToolbar, 
-            CommonModule, 
-            FormsModule,
-            IonInfiniteScroll,
-            HeaderComponent,
-          ]
+  imports: [IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonInfiniteScroll,
+    HeaderComponent,
+    IonList, IonLabel, IonItem,
+    IonInfiniteScrollContent,
+  
+  ]
 })
 export class InfiniteScrollPage implements OnInit {
+
+  data: any[] = Array(20);
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loadData( ) {
+   
+    setTimeout(() => {
+
+      if (this.data.length > 50) {
+        this.infiniteScroll.complete();
+        this.infiniteScroll.disabled = true;
+        return;
+      }
+
+      const nuevosDatos = Array(20);
+      this.data.push(...nuevosDatos);
+
+      this.infiniteScroll.complete();
+    },1500);
   }
 
 }
