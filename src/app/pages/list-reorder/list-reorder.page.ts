400ba6127179494970aea65e84a9fe7d78d70875
Angel David <angeldivad.100089@gmail.com>
2025-12-29 18:53:48 -0600

    list reorder

--- src/app/pages/list-reorder/list-reorder.page.ts
+++ src/app/pages/list-reorder/list-reorder.page.ts
@@ -1,28 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonButton } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { IonReorderGroup, IonItem, IonLabel, IonReorder,IonButtons} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-list-reorder',
   templateUrl: './list-reorder.page.html',
   styleUrls: ['./list-reorder.page.scss'],
   standalone: true,
-  imports: [IonContent, 
-            IonHeader, 
-            IonTitle, 
-            IonToolbar, 
-            CommonModule, 
-            FormsModule,
-            HeaderComponent,
-          ]
+  imports: [IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    IonReorder,
+    IonReorderGroup,
+    IonList,
+    IonItem, IonLabel, IonButton, IonButtons]
 })
 export class ListReorderPage implements OnInit {
 
+  personajes: string[]= ['Capibara','Marmota','Hamster','Cuyo','Hipopotamo']
+
+  reorderDisabled: boolean= true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  doReorder(event: any){
+    console.log(event);
+
+    const itemMover= this.personajes.splice(event.detail.from,1)[0];
+    this.personajes.splice(event.detail.to,0, itemMover);
+
+    event.detail.complete();
+    console.log(this.personajes);
+  }
 }
