bca5afaa419a129e2ba540e8672f31afcc3fdaf5
Angel David <angeldivad.100089@gmail.com>
2025-12-29 18:26:26 -0600

    list

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonListHeader } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
-import { IonList, IonItem, IonLabel,  } from '@ionic/angular/standalone';
-import { IonicModule } from "@ionic/angular";
-
-
-
+import { IonList, IonItem, IonLabel, IonItemSliding, IonItemOptions, IonItemOption, IonIcon } from '@ionic/angular/standalone';
+import { heartOutline, shareOutline, closeOutline, trashOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 
 @Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-list',
   templateUrl: './list.page.html',
   styleUrls: ['./list.page.scss'],
@@ -26,24 +25,50 @@ import { IonicModule } from "@ionic/angular";
     HeaderComponent,
     IonList,
     IonItem,
-    IonLabel, IonicModule]
+    IonLabel,
+    IonItemSliding,
+    IonItemOption,
+    IonItemOptions,
+    IonIcon,
+  ]
 })
 export class ListPage implements OnInit {
 
 usuarios: Observable<any> = new Observable();
 
-  constructor( private dataService: DataService ) { }
+@ViewChild(IonList) ionList!: IonList;
+
+  constructor( private dataService: DataService ) {
+    addIcons({ 'heart-outline': heartOutline, 
+              'share-outline': shareOutline,
+              'close-outline': closeOutline, 
+              'trash-outline':trashOutline });
+
+   }
 
   ngOnInit() {
-
     this.usuarios = this.dataService.getUsuarios();
-
   }
 
-  favorite( user: any ) {
-    console.log('favorite', user);
+  favorite(usuario: any, sliding: IonItemSliding) {
+    sliding.close();
+    (document.activeElement as HTMLElement)?.blur();
+    console.log('favorite', usuario);
+    this.ionList.closeSlidingItems();
   }
-  share( user: any ) {
-    console.log('share', user);
+
+  share(usuario: any, sliding: IonItemSliding) {
+    sliding.close();
+    (document.activeElement as HTMLElement)?.blur();
+    console.log('share', usuario);
+    this.ionList.closeSlidingItems();
   }
+
+  delete(usuario: any, sliding: IonItemSliding) {
+    sliding.close();
+    (document.activeElement as HTMLElement)?.blur();
+    console.log('delete', usuario.name);
+    this.ionList.closeSlidingItems();
+
+  }  
 }
