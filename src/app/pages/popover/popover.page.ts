2a1dc685b03b5330a2f30c5ce434d09f6210b155
Angel David <angeldivad.100089@gmail.com>
2026-01-02 11:33:40 -0600

    popover

--- src/app/pages/popover/popover.page.ts
+++ src/app/pages/popover/popover.page.ts
@@ -1,28 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonBackButton, IonButtons, IonIcon, IonFooter } from '@ionic/angular/standalone';
+
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { PopoverController } from '@ionic/angular';
+import { PopoverInfoComponent } from '../../components/popover-info/popover-info.component';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.page.html',
   styleUrls: ['./popover.page.scss'],
   standalone: true,
-  imports: [IonContent, 
-            IonHeader, 
-            IonTitle, 
-            IonToolbar, 
-            CommonModule, 
-            FormsModule,
-            HeaderComponent,
-          ]
+  imports: [IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonButton,
+    CommonModule,
+    FormsModule,
+    HeaderComponent, IonBackButton,
+    IonButtons,
+    IonFooter,
+    IonIcon
+    
+  ]
 })
 export class PopoverPage implements OnInit {
 
-  constructor() { }
+  constructor( private popoverCtrl: PopoverController) { }
 
   ngOnInit() {
+  }
+
+
+  async presentPopover( ev: any){
+    const popover = await this.popoverCtrl.create({
+      component: PopoverInfoComponent,
+      event: ev,
+      translucent: true,
+      backdropDismiss: false
+    });
+    return await popover.present();
   }
 
 }
