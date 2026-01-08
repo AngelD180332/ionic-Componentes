6c7a164fc314fefdde41b5b366be54cd6efc73aa
Angel David <angeldivad.100089@gmail.com>
2026-01-08 14:06:19 -0600

    android studio pruebas

--- src/app/pages/toast/toast.page.ts
+++ src/app/pages/toast/toast.page.ts
@@ -3,56 +3,65 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
 import { HeaderComponent } from 'src/app/components/header/header.component';
-import {ToastCon}
-
+import { ToastController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.page.html',
   styleUrls: ['./toast.page.scss'],
   standalone: true,
-  imports: [IonContent,
+  imports: [
+    IonContent,
     IonHeader,
     IonTitle,
     IonToolbar,
     CommonModule,
     FormsModule,
-    HeaderComponent, IonButton]
+    HeaderComponent, 
+    IonButton
+  ]
 })
 export class ToastPage implements OnInit {
 
-  constructor( private toastCtrl: ToastController) { }
+  constructor(private toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
-  async presentToast(){
+
+  async presentToast() {
     const toast = await this.toastCtrl.create({
-      message: 'Your settings have seen saved',
+      message: 'Your settings have been saved',
       duration: 2000
     });
+    
+    await toast.present();
   }
 
-    async presentToastingWithOpcions(){
+  async presentToastingWithOpcions() {
     const toast = await this.toastCtrl.create({
       header: 'Toast header',
       message: 'Click to close',
-      position:'top',
-      buttons:{
-        side: 'start',
-        icon: 'star',
-        text: 'Favorite',
-        handler:() => {
-          console.log('Favorite clicked');
-        }
-        }, {
-          text:'Done',
-          role:'Cancel',
-          handler:() => {
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, 
+        {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
             console.log('Cancel clicked');
           }
         }
+      ]
     });
+    
+    await toast.present();
   }
-
 
 }
